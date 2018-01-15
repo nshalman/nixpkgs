8fe1c89b8cbd3baa7c0dbaa806c546363f4840da
Nahum Shalman <nshalman@omniti.com>
2018-01-15 23:55:49 +0000

    need glide-0.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1173,6 +1173,8 @@ with pkgs;
 
   glide = callPackage ../development/tools/glide { };
 
+  glide10 = callPackage ../development/tools/glide/glide10.nix { };
+
   glock = callPackage ../development/tools/glock { };
 
   glslviewer = callPackage ../development/tools/glslviewer {
